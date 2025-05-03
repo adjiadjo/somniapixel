6a4c5c92d54a04e359fd075c97165f7666480614
adjiadjo <ilmansyahap@gmail.com>
2025-05-03 12:39:16 +0700

    Update somniapixel.py

--- somniapixel.py
+++ somniapixel.py
@@ -46,8 +46,7 @@ for i in range(loop_count):
 
     signed_txn = w3.eth.account.sign_transaction(txn, private_key=private_key)
     
-    # Use signed_txn['rawTransaction'] instead of signed_txn.rawTransaction
-    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    tx_hash = w3.eth.send_raw_transaction(signed_txn['rawTransaction'])
 
     # Print the transaction hash
     print(f"Transaction Hash: {tx_hash.hex()}")
